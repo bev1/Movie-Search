dbaa1d4ffb66570fe190ca2a650c279c64504d96
bev1 <longer30001@gmail.com>
2019-09-07 19:38:07 +0300

    Updates

---
+++ precache-manifest.0fcd894c2e8e4d3133bd5a259a584cd5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c509f732df8fb3430563cc5dbe8708ff",
+    "url": "/Movie-Search/index.html"
+  },
+  {
+    "revision": "48865348ba88e3d87721",
+    "url": "/Movie-Search/static/css/2.bd2a69fd.chunk.css"
+  },
+  {
+    "revision": "48865348ba88e3d87721",
+    "url": "/Movie-Search/static/js/2.7d0c97d1.chunk.js"
+  },
+  {
+    "revision": "a2fedc1a2032bc634c9d",
+    "url": "/Movie-Search/static/js/main.7ab4ec50.chunk.js"
+  },
+  {
+    "revision": "f7cf3e4a4237e9fc7fba",
+    "url": "/Movie-Search/static/js/runtime~main.d5b6df03.js"
+  },
+  {
+    "revision": "b4ce5e46433c3ed6be5ece9d56019b50",
+    "url": "/Movie-Search/static/media/bar.b4ce5e46.svg"
+  },
+  {
+    "revision": "aa194c920bdd9c27ab8665ca3e95c34b",
+    "url": "/Movie-Search/static/media/circle.aa194c92.svg"
+  },
+  {
+    "revision": "817d8c3d24ffde54e48f901cb39578ba",
+    "url": "/Movie-Search/static/media/clock.817d8c3d.svg"
+  },
+  {
+    "revision": "d25462bbe659a069ae46f54165b1e272",
+    "url": "/Movie-Search/static/media/dots.d25462bb.svg"
+  },
+  {
+    "revision": "9c99ea64ee60621e09d7b689ba3c8169",
+    "url": "/Movie-Search/static/media/double-circle.9c99ea64.svg"
+  },
+  {
+    "revision": "b22c1583df127e277d48fdba8dd6b347",
+    "url": "/Movie-Search/static/media/drop.b22c1583.svg"
+  },
+  {
+    "revision": "23cbd0aedea5d799f3312fb2de0d61c2",
+    "url": "/Movie-Search/static/media/spinner.23cbd0ae.svg"
+  },
+  {
+    "revision": "261f22ef50f18c010da606027aee091c",
+    "url": "/Movie-Search/static/media/star_grey.261f22ef.png"
+  },
+  {
+    "revision": "1c7624aa5d277d6d57a034a82d78a8d3",
+    "url": "/Movie-Search/static/media/star_yellow.1c7624aa.png"
+  }
+]);
